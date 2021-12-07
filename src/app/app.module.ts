fc50f8db35993c5c2bd0165318c57cd422cf8b42
aleebit <alee@madisoncollege.edu>
2021-12-07 10:51:44 -0600

    create 2 components
    ng generate component greeting  --skip-tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WorldComponent } from './world/world.component';
+import { KittyComponent } from './kitty/kitty.component';
+import { SharedDataComponent } from './shared-data/shared-data.component';
+import { GreetingComponent } from './greeting/greeting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorldComponent,
+    KittyComponent,
+    SharedDataComponent,
+    GreetingComponent
   ],
   imports: [
     BrowserModule,
